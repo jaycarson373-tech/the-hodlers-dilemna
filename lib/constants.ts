4fa83067d28266b38446f3714cfa1c288caf61e8
jaycarson373-tech <263076219+jaycarson373-tech@users.noreply.github.com>
2026-07-27 15:06:43 -0400

    Clear launch CA and restore 1M minimum

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const TICKER = "$BINGO";
-export const CA = "EVJSSzoD73noecpLV6f2Y3589AxV6XK3vFEV1vGNpump";
+export const CA = process.env.NEXT_PUBLIC_TOKEN_MINT?.trim() || "";
 export const SITE_URL = "https://www.bingopump.fun";
-export const PUMP_FUN_URL = `https://pump.fun/coin/${CA}`;
-export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CA}`;
+export const PUMP_FUN_URL = CA ? `https://pump.fun/coin/${CA}` : "";
+export const DEXSCREENER_URL = CA ? `https://dexscreener.com/solana/${CA}` : "";
 export const COMMUNITY_URL = "https://x.com/i/communities/2028470502415835347";
 export const X_URL = "https://x.com/bingopumpfun";
